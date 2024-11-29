12ce03eade4a9f1b5a3c71ee8fa4be8ffb113ff7
TAN KOK WEE 2303986J <2303986J@student.tp.edu.sg>
2024-11-29 15:08:57 +0800

    Most updated car rental management

--- CarRentalManagement/Domain/Vehicle.cs
+++ CarRentalManagement/Domain/Vehicle.cs
@@ -4,8 +4,8 @@
 	{
 		public int	Year { get; set; }
 		public string? LicensePlateNumber { get; set; }
-		public int? MakeId { get; set; }
-		public int? ModelId { get; set; }
-		public int? ColourId { get; set; }
+		public int MakeId { get; set; }
+		public int ModelId { get; set; }
+		public int ColourId { get; set; }
 	}
 }
